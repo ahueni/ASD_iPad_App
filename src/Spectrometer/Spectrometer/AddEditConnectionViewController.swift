2ef377a8e57252e4cf242c1fc113b714aaf09eed
Raphael Bolliger <raphael.bolliger@students.fhnw.ch>
2016-11-19 17:03:24 +0100

    added font open sans / created some views

---
+++ src/Spectrometer/Spectrometer/AddEditConnectionViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddEditConnectionViewController.swift
+//  Spectrometer
+//
+//  Created by raphi on 19.11.16.
+//  Copyright © 2016 YARX GmbH. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddEditConnectionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func openFileBrowser(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+    
+}
