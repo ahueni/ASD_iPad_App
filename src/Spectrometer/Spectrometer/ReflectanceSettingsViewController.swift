0f79a967c65bbfcd64fa299bcf235e0444d5c82e
Raphael Bolliger <raphael.bolliger@students.fhnw.ch>
2017-02-16 11:32:33 +0100

    finished reflectance settings view

--- src/Spectrometer/Spectrometer/ReflectanceSettingsViewController.swift
+++ src/Spectrometer/Spectrometer/ReflectanceSettingsViewController.swift
@@ -11,6 +11,40 @@ import UIKit
 
 class ReflectanceSettingsViewController : BaseMeasurementModal {
     
+    // MARK: WhiteReference settings
+    @IBOutlet var darkCurrentSettingsContentHeight: NSLayoutConstraint!
+    
+    @IBAction func darkCurrentSettingsSwitchValueChanged(_ sender: UISwitch) {
+        
+        if sender.isOn {
+            self.darkCurrentSettingsContentHeight.constant = 60
+        } else {
+            self.darkCurrentSettingsContentHeight.constant = 0
+        }
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.view.layoutIfNeeded()
+        })
+        
+    }
+    
+    // MARK: Target settings
+    @IBOutlet var targetCountLabel: UILabel!
+    @IBOutlet var targetIntervallLabel: UILabel!
+    
+    
+    @IBAction func targetCountStepperValueChanged(_ sender: UIStepper) {
+        targetCountLabel.text = Int(sender.value).description
+    }
+    
+    @IBAction func targetIntervallStepperValueChanged(_ sender: UIStepper) {
+        targetIntervallLabel.text = Int(sender.value).description
+    }
+    
+    
+    
+    
+    
 }
 
 
